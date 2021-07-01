da1280273170328c61e92b013626d12c01fac952
Taliesin Beynon <github@taliesin.ai>
2021-07-01 12:42:56 +0200

    * Path algebra:   * Reorder definitions a bit   * RandomPathField, RandomPathVector, SparsifyPathVector.

--- Kernel/PathAlgebra.m
+++ Kernel/PathAlgebra.m
@@ -85,6 +85,7 @@ PathAlgebra[quiver_?GraphQ, field_, OptionsPattern[]] ? System`Private`HoldEntry
   data["NullVertex"] = dummy = count + 1;
   tagOutTable = Append[dummy] /@ ReplaceAll[TagVertexOutTable @ quiver, None -> dummy];
   data["TagOutTable"] = tagOutTable;
+  data["AdjacencyTable"] = VertexAdjacencyTable @ quiver;
 
   System`Private`ConstructNoEntry[PathAlgebra, data]
 ];
@@ -121,128 +122,6 @@ PathAlgebraBoxes[object:PathAlgebra[data_], form_] := Scope[
 
 (**************************************************************************************************)
 
-PackageExport["PathBivector"]
-
-SetUsage @ "
-PathBivector[$$] represents a path bivector on a cardinal quiver.
-"
-
-PathBivector[a_PathVector] :=
-  PathBivector[a, a];
-
-declareFormatting[
-  bv:PathBivector[_PathVector, _PathVector] /; $PathAlgebra =!= None :>
-    formatPathBivector[bv]
-];
-
-formatPathBivector[PathBivector[a_PathVector, b_PathVector]] :=
-  AngleBracket[a, b];
-
-(**************************************************************************************************)
-
-PackageExport["PathBivectorQ"]
-
-PathBivectorQ[PathBivector[a_ ? PathVectorQ, b_ ? PathVectorQ]] := True;
-PathBivectorQ[_] := False;
-
-declarePlusTimesDispatch[PathBivector, PathBivectorQ, $pathBivectorDispatch]
-
-$pathBivectorDispatch = <|
-  Plus -> PathBivectorPlus,
-  Times -> PathBivectorTimes
-|>;
-
-PackageExport["PathBivectorPlus"]
-PackageExport["PathBivectorTimes"]
-
-PathBivectorPlus[a_, b_, c_] := PathBivectorPlus[PathBivectorPlus[a, b], c];
-
-PathBivectorPlus[PathBivector[a_, b_], PathBivector[c_, d_]] :=
-  PathBivector[PathVectorPlus[a, c], PathVectorPlus[b, d]];
-
-PathBivectorTimes[PathBivector[a_, b_], PathBivector[c_, d_]] :=
-  PathBivector[PathVectorTimes[a, c], PathVectorTimes[b, d]];
-
-PathBivectorTimes[n_ ? NumericQ, PathBivector[a_, b_]] := PathBivector[
-  PathVectorTimes[n, a],
-  PathVectorTimes[n, b]
-];
-
-(**************************************************************************************************)
-
-PackageExport["ConvolutionBivector"]
-
-SetUsage @ "
-ConvolutionBivector[vector$] gives the Bivector[$$] that achieves a convolution with vector$.
-"
-
-ConvolutionBivector[pv_PathVector] :=
-  PathBivector[pv, pv];
-
-(**************************************************************************************************)
-
-PackageExport["TranslationBivector"]
-
-SetUsage @ "
-TranslationBivector[word$] gives the Bivector[$$] that achieves the translation by the cardinal word.
-"
-
-TranslationBivector[w_String] :=
-  ConvolutionBivector @ WordVector @ w;
-
-(**************************************************************************************************)
-
-PackageExport["PathConvolution"]
-
-SetUsage @ "
-PathConvolution[k$, v$] gives the resulting of convolving path kernel k$ with v$.
-"
-
-PathConvolution[k_PathVector, v_PathVector] := PathCompose[k, PathCompose[v, PathReverse @ k]]
-
-(**************************************************************************************************)
-
-PackageExport["Sandwich"]
-
-SetUsage @ "
-Sandwich[bivector$, vector$] computes the sandwich product of a bivector with a vector.
-"
-
-Sandwich[PathBivector[l_PathVector, r_PathVector], PathVector[m_Association]] /; $PathAlgebraQ := Scope[
-  s = Normal @ First @ (l + r);
-  a = Normal @ First @ (l - r);
-  m = Normal @ m;
-  UnpackPathAlgebra[fieldPlus];
-  temp = None;
-  z1 = constructPathVector @ mergeWeights[Outer[symmetricSandwichProduct, s, m], 0, Apply @ fieldPlus];
-  z2 = constructPathVector @ mergeWeights[Outer[symmetricSandwichProduct, a, m], 0, Apply @ fieldPlus];
-  z1 - PathReverse @ z2
-];
-
-symmetricSandwichProduct[s:PathElement[sv_] -> sw_, m:PathElement[mv_] -> mw_] := (
-  temp = PathCompose[s, m, PathReverse @ s];
-  If[temp === NullElement, Nothing, temp -> (sw * mw)/2]
-)
-
-PathBivector /: CircleDot[a_PathBivector, v_PathVector] := Sandwich[a, v];
-
-(**************************************************************************************************)
-
-PackageExport["PathVectorQ"]
-
-SetUsage @ "
-PathVectorQ[expr$] gives True if expr$ is a valid PathVector.
-"
-
-PathVectorQ[PathVector[_Association]] := $PathAlgebraQ;
-PathVectorQ[_] := False;
-
-
-PathVectorVectorQ[{PathVector[_Association]...}] := $PathAlgebraQ;
-PathVectorVectorQ[_] := False;
-
-(**************************************************************************************************)
-
 PackageExport["PathVector"]
 
 SetUsage @ "
@@ -276,70 +155,25 @@ PathVectorElementwise[f_, id_, vecs_] := Scope[
 mergeWeights[rules_, id_, f_] :=
   Merge[KeyUnion[Flatten @ rules, id&], f];
 
-constructPathVector[assoc_] :=
+constructPathVector[list_List] := constructPathVector @ Association @ list;
+
+constructPathVector[assoc_Association] :=
   PathVector @ Association @ KeySort @ KeyDrop[NullElement] @ DeleteCases[assoc, 0|0.];
 
 (**************************************************************************************************)
 
-PackageExport["PathHead"]
-PackageExport["PathTail"]
+PackageExport["PathVectorQ"]
 
-PathTail[PathElement[list_List]] := First @ list;
-PathHead[PathElement[list_List]] := Last @ list;
+SetUsage @ "
+PathVectorQ[expr$] gives True if expr$ is a valid PathVector.
+"
 
-(**************************************************************************************************)
-
-PackageExport["NullPath"]
-PackageExport["NullElement"]
-
-declareFormatting[
-  NullPath :> "\[UpTee]"
-  NullElement :> "\[UpTee]"
-];
+PathVectorQ[PathVector[_Association]] := $PathAlgebraQ;
+PathVectorQ[_] := False;
 
 
-(**************************************************************************************************)
-
-PathVector /: CenterDot[v__PathVector] := PathCompose[v];
-
-PathBivector /: CenterDot[b__PathBivector] := PathCompose[b];
-
-(**************************************************************************************************)
-
-PackageExport["PathCompose"]
-
-$cancelRules = Dispatch @ {
-  {l___, i_, j_, i_, r___} :> {l, i, r}
-};
-
-compose[a_, b_] := PathElement[Join[a, Rest @ b] //. $cancelRules];
-
-compose[a_, b_, c_] := PathElement[Join[a, Rest @ b, Rest @ c] //. $cancelRules];
-
-PathCompose[e_] := e;
-
-PathCompose[PathElement[a_], PathElement[b_], PathElement[c_]] :=
-  If[Last[a] === First[b] && Last[b] === First[c], compose[a, b, c], NullElement];
-
-PathCompose[PathElement[a_], PathElement[b_]] :=
-  If[Last[a] === First[b], compose[a, b], NullPath];
-
-PathCompose[a_PathVector, b_PathVector, c_PathVector, rest___PathVector] /; $PathAlgebraQ :=
-  PathCompose[PathCompose[PathCompose[a, b], c], rest];
-
-PathCompose[PathVector[a1_Association], PathVector[a2_Association]] /; $PathAlgebraQ := Scope[
-  $times = $FieldTimes;
-  constructPathVector @ Merge[
-    Flatten @ Outer[composeWeightedPaths, Normal @ a1, Normal @ a2, 1],
-    Apply @ $FieldPlus
-  ]
-]
-
-PathCompose[PathBivector[l1_, r1_], PathBivector[l2_, r2_]] /; $PathAlgebraQ :=
-  PathBivector[PathCompose[l1, l2], PathCompose[r1, r2]]
-
-composeWeightedPaths[PathElement[a_] -> aw_, PathElement[b_] -> bw_] :=
-  If[Last[a] === First[b], compose[a, b] -> $times[aw, bw], {}];
+PathVectorVectorQ[{PathVector[_Association]...}] := $PathAlgebraQ;
+PathVectorVectorQ[_] := False;
 
 (**************************************************************************************************)
 
@@ -449,6 +283,68 @@ fieldColors = MatchValues[
   4 := {$Gray, $Red, $DarkRed, $DarkBlue, $Blue};
   n_ := Take[Prepend[$ColorPalette, White], n];
 ];
+
+(**************************************************************************************************)
+
+PackageExport["PathHead"]
+PackageExport["PathTail"]
+
+PathTail[PathElement[list_List]] := First @ list;
+PathHead[PathElement[list_List]] := Last @ list;
+
+(**************************************************************************************************)
+
+PackageExport["NullPath"]
+PackageExport["NullElement"]
+
+declareFormatting[
+  NullPath :> "\[UpTee]"
+  NullElement :> "\[UpTee]"
+];
+
+
+(**************************************************************************************************)
+
+PathVector /: CenterDot[v__PathVector] := PathCompose[v];
+
+PathBivector /: CenterDot[b__PathBivector] := PathCompose[b];
+
+(**************************************************************************************************)
+
+PackageExport["PathCompose"]
+
+$cancelRules = Dispatch @ {
+  {l___, i_, j_, i_, r___} :> {l, i, r}
+};
+
+compose[a_, b_] := PathElement[Join[a, Rest @ b] //. $cancelRules];
+
+compose[a_, b_, c_] := PathElement[Join[a, Rest @ b, Rest @ c] //. $cancelRules];
+
+PathCompose[e_] := e;
+
+PathCompose[PathElement[a_], PathElement[b_], PathElement[c_]] :=
+  If[Last[a] === First[b] && Last[b] === First[c], compose[a, b, c], NullElement];
+
+PathCompose[PathElement[a_], PathElement[b_]] :=
+  If[Last[a] === First[b], compose[a, b], NullPath];
+
+PathCompose[a_PathVector, b_PathVector, c_PathVector, rest___PathVector] /; $PathAlgebraQ :=
+  PathCompose[PathCompose[PathCompose[a, b], c], rest];
+
+PathCompose[PathVector[a1_Association], PathVector[a2_Association]] /; $PathAlgebraQ := Scope[
+  $times = $FieldTimes;
+  constructPathVector @ Merge[
+    Flatten @ Outer[composeWeightedPaths, Normal @ a1, Normal @ a2, 1],
+    Apply @ $FieldPlus
+  ]
+]
+
+PathCompose[PathBivector[l1_, r1_], PathBivector[l2_, r2_]] /; $PathAlgebraQ :=
+  PathBivector[PathCompose[l1, l2], PathCompose[r1, r2]]
+
+composeWeightedPaths[PathElement[a_] -> aw_, PathElement[b_] -> bw_] :=
+  If[Last[a] === First[b], compose[a, b] -> $times[aw, bw], {}];
 
 (**************************************************************************************************)
 
@@ -594,7 +490,7 @@ RandomVertexField[] constructs a random vertex field, containing every empty pat
 
 RandomVertexField[] /; $PathAlgebraQ := Scope[
   UnpackPathAlgebra[vertexList, randomFieldElement];
-  constructPathVector @ Association @ Map[v |-> PathElement[{v}] -> randomFieldElement[], vertexList]
+  constructPathVector @ Map[v |-> PathElement[{v}] -> randomFieldElement[], vertexList]
 ];
 
 (**************************************************************************************************)
@@ -621,8 +517,75 @@ RandomEdgeField[type_:"Symmetric"] /; $PathAlgebraQ := Scope[
     "Reverse", reverse,
     "Symmetric" | "Antisymmetric", Join[forward, reverse]
   ];
-  constructPathVector @ Association @ Map[elem |-> elem -> randomFieldElement[], pairs]
+  constructPathVector @ Map[elem |-> elem -> randomFieldElement[], pairs]
 ];
+
+(**************************************************************************************************)
+
+PackageExport["RandomPathField"]
+
+SetUsage @ "
+RandomPathField[] constructs a random path field of paths of length 0 \[LessEqual] n$ \[LessEqual] 2.
+RandomPathField[n$] chooses paths to have length n$.
+RandomPathField[{n$min, n$max}] chooses paths to have length n$, where n$min \[LessEqual] n$ \[LessEqual] n$max.
+* The random weights are chosen from the appropriate base field.
+* For finite fields, any value is equally likely to be chosen.
+* For infinite fields, the range [-2, 2] is used.
+* Only one path will start at a given vertex (hence the name 'field').
+* RandomPathField[spec$] is equivalent to RandomPathVector[1, spec$].
+"
+
+RandomPathField[] :=
+  RandomPathVector[1];
+
+RandomPathField[range:{_Integer ? NonNegative, _Integer ? NonNegative} | (_Integer ? NonNegative)] :=
+  RandomPathVector[1, range];
+
+(**************************************************************************************************)
+
+PackageExport["SparsifyPathVector"]
+
+SetUsage @ "
+SparsifyPathVector[vector$, frac$] deletes frac$ of the paths in a %PathVector.
+"
+
+SparsifyPathVector[PathVector[assoc_], frac_Real] :=
+  PathVector @ Select[assoc, RandomReal[] > frac&]
+
+(**************************************************************************************************)
+
+PackageExport["RandomPathVector"]
+
+SetUsage @ "
+RandomPathVector[m$] constructs a random path vector of paths of length 0 \[LessEqual] n$ \[LessEqual] 2, with m$ starting at each vertex.
+RandomPathVector[m$, n$] chooses paths to have length n$.
+RandomPathVector[m$, {n$min, n$max}] chooses paths to have length n$, where n$min \[LessEqual] n$ \[LessEqual] n$max.
+* Unlike RandomPathField, RandomPathVector explicitly does construct paths that start at the same vertex.
+* The random weights are chosen from the appropriate base field.
+* For finite fields, any value is equally likely to be chosen.
+* For infinite fields, the range [-2, 2] is used.
+* RandomPathVector[1, spec$] is equivalent to RandomPathField[spec$].
+"
+
+RandomPathVector[m_Integer] :=
+  RandomPathVector[m, {0, 2}];
+
+RandomPathVector[m_Integer, range_Integer] := RandomPathVector[m, {range, range}];
+
+RandomPathVector[m_Integer, range:{_Integer ? NonNegative, _Integer ? NonNegative}] := Scope[
+  UnpackPathAlgebra[vertexList, adjacencyTable, randomFieldElement];
+  constructPathVector @ Map[
+    v |-> Table[
+        len = RandomInteger[range];
+        PathElement[NestList[chooseNext, v, len]] -> randomFieldElement[]
+      ,
+        {m}
+      ],
+    vertexList
+  ]
+];
+
+chooseNext[v_] := RandomChoice @ Part[adjacencyTable, v]
 
 (**************************************************************************************************)
 
@@ -705,3 +668,110 @@ PackageExport["EdgeFieldQ"]
 EdgeFieldQ[PathVector[assoc_Association]] := MatchQ[Keys @ assoc, {PathElement[{_, _}]...}];
 EdgeFieldQ[_] := False;
 
+
+(**************************************************************************************************)
+
+PackageExport["PathBivector"]
+
+SetUsage @ "
+PathBivector[$$] represents a path bivector on a cardinal quiver.
+"
+
+PathBivector[a_PathVector] :=
+  PathBivector[a, a];
+
+declareFormatting[
+  bv:PathBivector[_PathVector, _PathVector] /; $PathAlgebra =!= None :>
+    formatPathBivector[bv]
+];
+
+formatPathBivector[PathBivector[a_PathVector, b_PathVector]] :=
+  AngleBracket[a, b];
+
+(**************************************************************************************************)
+
+PackageExport["PathBivectorQ"]
+
+PathBivectorQ[PathBivector[a_ ? PathVectorQ, b_ ? PathVectorQ]] := True;
+PathBivectorQ[_] := False;
+
+declarePlusTimesDispatch[PathBivector, PathBivectorQ, $pathBivectorDispatch]
+
+$pathBivectorDispatch = <|
+  Plus -> PathBivectorPlus,
+  Times -> PathBivectorTimes
+|>;
+
+PackageExport["PathBivectorPlus"]
+PackageExport["PathBivectorTimes"]
+
+PathBivectorPlus[a_, b_, c_] := PathBivectorPlus[PathBivectorPlus[a, b], c];
+
+PathBivectorPlus[PathBivector[a_, b_], PathBivector[c_, d_]] :=
+  PathBivector[PathVectorPlus[a, c], PathVectorPlus[b, d]];
+
+PathBivectorTimes[PathBivector[a_, b_], PathBivector[c_, d_]] :=
+  PathBivector[PathVectorTimes[a, c], PathVectorTimes[b, d]];
+
+PathBivectorTimes[n_ ? NumericQ, PathBivector[a_, b_]] := PathBivector[
+  PathVectorTimes[n, a],
+  PathVectorTimes[n, b]
+];
+
+(**************************************************************************************************)
+
+PackageExport["ConvolutionBivector"]
+
+SetUsage @ "
+ConvolutionBivector[vector$] gives the Bivector[$$] that achieves a convolution with vector$.
+"
+
+ConvolutionBivector[pv_PathVector] :=
+  PathBivector[pv, pv];
+
+(**************************************************************************************************)
+
+PackageExport["TranslationBivector"]
+
+SetUsage @ "
+TranslationBivector[word$] gives the Bivector[$$] that achieves the translation by the cardinal word.
+"
+
+TranslationBivector[w_String] :=
+  ConvolutionBivector @ WordVector @ w;
+
+(**************************************************************************************************)
+
+PackageExport["PathConvolution"]
+
+SetUsage @ "
+PathConvolution[k$, v$] gives the resulting of convolving path kernel k$ with v$.
+"
+
+PathConvolution[k_PathVector, v_PathVector] := PathCompose[k, PathCompose[v, PathReverse @ k]]
+
+(**************************************************************************************************)
+
+PackageExport["Sandwich"]
+
+SetUsage @ "
+Sandwich[bivector$, vector$] computes the sandwich product of a bivector with a vector.
+"
+
+Sandwich[PathBivector[l_PathVector, r_PathVector], PathVector[m_Association]] /; $PathAlgebraQ := Scope[
+  s = Normal @ First @ (l + r);
+  a = Normal @ First @ (l - r);
+  m = Normal @ KeySelect[m, PathHead[#] === PathTail[#]&];
+  UnpackPathAlgebra[fieldPlus];
+  temp = None;
+  z1 = constructPathVector @ mergeWeights[Outer[symmetricSandwichProduct, s, m], 0, Apply @ fieldPlus];
+  z2 = constructPathVector @ mergeWeights[Outer[symmetricSandwichProduct, a, m], 0, Apply @ fieldPlus];
+  z1 + z2
+];
+
+symmetricSandwichProduct[s:PathElement[sv_] -> sw_, m:PathElement[mv_] -> mw_] := (
+  temp = PathCompose[s, m, PathReverse @ s];
+  If[temp === NullElement, Nothing, temp -> (sw * mw)/2]
+)
+
+PathBivector /: CircleDot[a_PathBivector, v_PathVector] := Sandwich[a, v];
